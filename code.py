561d3d18a994f731946d9d2b0e3b5790f601b27b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-12-25 05:03:04 -0500

    added a poller that looks for messages from adafruit.io feed

--- code.py
+++ code.py
@@ -1,53 +1,100 @@
-import ipaddress
-import ssl
-import wifi
-import socketpool
-import adafruit_requests
-import secrets
+import time
+import board
+import busio
+from digitalio import DigitalInOut
+from adafruit_esp32spi import adafruit_esp32spi
+from adafruit_esp32spi import adafruit_esp32spi_wifimanager
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+import neopixel
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
+from adafruit_io.adafruit_io import IO_MQTT
 
-
-TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
-JSON_QUOTES_URL = "https://www.adafruit.com/api/quotes.php"
-JSON_STARS_URL = "https://api.github.com/repos/adafruit/circuitpython"
-
-# Get wifi details and more from a secrets.py file
+# load secrets
 try:
     from secrets import secrets
 except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-# Get our username, key and desired timezone
-aio_username = secrets["aio_username"]
-aio_key = secrets["aio_key"]
-location = secrets.get("timezone", None)
-TIME_URL = "https://io.adafruit.com/api/v2/%s/integrations/time/strftime?x-aio-key=%s" % (
-    aio_username, aio_key)
-TIME_URL += "&fmt=%25Y-%25m-%25d+%25H%3A%25M%3A%25S.%25L+%25j+%25u+%25z+%25Z"
 
-print("ESP32-S2 Adafruit IO Time test")
+# setup pins and hardware
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
+wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(
+    esp, secrets, status_light
+)
 
-print("My MAC addr:", [hex(i) for i in wifi.radio.mac_address])
+# setup IO callbacks for listening to a feed
 
-print("Available WiFi networks:")
-for network in wifi.radio.start_scanning_networks():
-    print("\t%s\t\tRSSI: %d\tChannel: %d" % (str(network.ssid, "utf-8"),
-                                             network.rssi, network.channel))
-wifi.radio.stop_scanning_networks()
 
-print("Connecting to %s" % secrets["ssid"])
-wifi.radio.connect(secrets["ssid"], secrets["password"])
-print("Connected to %s!" % secrets["ssid"])
-print("My IP address is", wifi.radio.ipv4_address)
+def connected(client):
+    print("Connected to Adafruit IO!")
 
-ipv4 = ipaddress.ip_address("8.8.4.4")
-print("Ping google.com: %f ms" % wifi.radio.ping(ipv4))
 
-pool = socketpool.SocketPool(wifi.radio)
-requests = adafruit_requests.Session(pool, ssl.create_default_context())
+def subscribe(client, userdata, topic, granted_qos):
+    print("Listening for changes on relay feed...")
 
-print("Fetching text from", TIME_URL)
-response = requests.get(TIME_URL)
-print("-" * 40)
-print(response.text)
-print("-" * 40)
+
+def unsubscribe(client, userdata, topic, pid):
+    print("Unsubscribed from {0} with PID {1}".format(topic, pid))
+
+
+def disconnected(client):
+    print("Disconnected from Adafruit IO!")
+
+
+def on_message(client, feed_id, payload):
+    print("Feed {0} received new value: {1}".format(feed_id, payload))
+
+
+def on_feed_msg(client, topic, message):
+    print(message)
+
+
+# connect to WiFi
+print("Connecting to WiFi...")
+wifi.connect()
+print("Connected!")
+
+# initialize MQTT interface with the esp interface
+MQTT.set_socket(socket, esp)
+
+# initialize a new MQTT Client object
+mqtt_client = MQTT.MQTT(
+    broker="io.adafruit.com",
+    username=secrets["aio_username"],
+    password=secrets["aio_key"],
+)
+
+# Initialize an Adafruit IO MQTT Client
+io = IO_MQTT(mqtt_client)
+
+# connect the callback methods defined above to Adafruit IO
+io.on_connect = connected
+io.on_disconnect = disconnected
+io.on_subscribe = subscribe
+io.on_unsubscribe = unsubscribe
+io.on_message = on_message
+
+# connect to Adafruit IO
+print("Connecting to Adafruit IO...")
+feed_name = "messages"
+io.connect()
+io.add_feed_callback(feed_name, on_feed_msg)
+io.subscribe(feed_name)
+io.get(feed_name)
+
+# loop looking for messages
+while True:
+    try:
+        io.loop()
+    except (ValueError, RuntimeError) as e:
+        print("Failed to get data, retrying\n", e)
+        wifi.reset()
+        io.reconnect()
+        continue
+    time.sleep(60)
