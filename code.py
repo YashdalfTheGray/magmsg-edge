42b7397e5c00d73cd500e890787a395cebd50472
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-08 03:30:07 -0500

    bit of reorganization because more and more things needed the contents of one function

--- code.py
+++ code.py
@@ -9,6 +9,10 @@ import terminalio
 from adafruit_magtag.magtag import MagTag
 
 
+def sortFunc(elem):
+    return elem["createdAt"]
+
+
 def get_battery_color(current_volts):
     battery_min = 3.5
     battery_max = 4.15
@@ -31,59 +35,31 @@ def get_battery_color(current_volts):
         return (255, int(255.0 * position), 0)
 
 
-def handle_buttons(magtag, messages, message_index):
-    result = message_index
-
-    if magtag.peripherals.button_a_pressed:
-        if result > 0:
-            result = result - 1 if result >= 1 else 0
-            magtag.set_text(messages[result]["content"])
-        magtag.peripherals.neopixel_disable = True
-    elif magtag.peripherals.button_b_pressed:
-        magtag.peripherals.neopixel_disable = False
-        magtag.peripherals.neopixels.fill((255, 255, 255))
-    elif magtag.peripherals.button_c_pressed:
-        magtag.peripherals.neopixel_disable = False
-        magtag.peripherals.neopixels.fill(
-            get_battery_color(magtag.peripherals.battery)
-        )
-    elif magtag.peripherals.button_d_pressed:
-        if result < (len(messages) - 1):
-            result = result + \
-                1 if result <= (len(messages) - 2) else (len(messages) - 1)
-            magtag.set_text(messages[result]["content"])
-        magtag.peripherals.neopixel_disable = True
-    else:
-        magtag.peripherals.neopixel_disable = True
-
-    return result
-
-
 try:
     from secrets import secrets
 except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
+
 auth_header = {
     "Authorization": "Bearer " + secrets["server_token"]
 }
+sleep_timer = 3000
+counter = 0
+
 
 print("Connecting to %s" % secrets["ssid"])
 wifi.radio.connect(secrets["ssid"], secrets["password"])
 print("Connected to %s!" % secrets["ssid"])
 print("My IP address is", wifi.radio.ipv4_address)
 
+
 pool = socketpool.SocketPool(wifi.radio)
 requests = adafruit_requests.Session(pool, ssl.create_default_context())
-
-
-def sortFunc(elem):
-    return elem["createdAt"]
-
-
 response = requests.request(
-    "GET", secrets["server_address"], headers=auth_header)
+    "GET", secrets["server_address"], headers=auth_header
+)
 messages = response.json()
 messages.sort(key=sortFunc)
 print(messages)
@@ -102,19 +78,43 @@ magtag.add_text(
 current_message_index = len(messages) - 1
 magtag.set_text(messages[current_message_index]["content"])
 
-buttons = magtag.peripherals.buttons
-button_colors = ((255, 0, 0), (255, 150, 0), (0, 255, 255), (180, 0, 255))
-timestamp = time.monotonic()
-
-counter = 0
-
 while True:
-    current_message_index = handle_buttons(
-        magtag, messages, current_message_index)
-
-    counter += 1
-
-    if (counter > 1000):
+    if magtag.peripherals.button_a_pressed:
+        if current_message_index > 0:
+            current_message_index = current_message_index - \
+                1 if current_message_index >= 1 else 0
+            magtag.set_text(messages[current_message_index]["content"])
+        magtag.peripherals.neopixel_disable = True
         counter = 0
 
+    elif magtag.peripherals.button_b_pressed:
+        magtag.peripherals.neopixel_disable = False
+        magtag.peripherals.neopixels.fill((255, 255, 255))
+        counter = 0
+
+    elif magtag.peripherals.button_c_pressed:
+        magtag.peripherals.neopixel_disable = False
+        magtag.peripherals.neopixels.fill(
+            get_battery_color(magtag.peripherals.battery)
+        )
+        counter = 0
+
+    elif magtag.peripherals.button_d_pressed:
+        if current_message_index < (len(messages) - 1):
+            current_message_index = current_message_index + \
+                1 if current_message_index <= (
+                    len(messages) - 2) else (len(messages) - 1)
+            magtag.set_text(messages[current_message_index]["content"])
+        magtag.peripherals.neopixel_disable = True
+        counter = 0
+
+    else:
+        magtag.peripherals.neopixel_disable = True
+        counter += 1
+
+    if counter > sleep_timer:
+        break
+
     time.sleep(0.1)
+
+print("No interaction detected, going to deep sleep")
